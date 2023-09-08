d8cdfd43d9aa2f363437443c95d9f791db721ee2
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-09-08 14:47:16 +0700

    add interface

--- internal/repository/adapter/adapter.go
+++ internal/repository/adapter/adapter.go
@@ -10,7 +10,13 @@ type Database struct {
 	logMode    bool
 }
 
-type Interface interface {}
+type Interface interface {
+	Health() bool
+	FindAll(condition expression.Expression, tableName string) (response *dynamodb.ScanOutput, err error)
+	FindOne(condition map[string]interface{}, tableName string) (response *dynamodb.GetItemOutput, err error)
+	CreateOrUpdate(entity interface{}, tableName string) (response *dynamodb.PutItemOutput, err error)
+	Delete(condition map[string]interface{}, tableName string) (response *dynamodb.DeleteItemOutput, err error)
+}
 
 
 func NewAdapter(con *dynamodb.DynamoDB) Interface {}
