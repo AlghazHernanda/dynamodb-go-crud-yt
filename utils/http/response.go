d7b8123507409ac60c6a4d99278cea57e0981f86
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-09-08 10:27:28 +0700

    json response

---
+++ utils/http/response.go
@@ -0,0 +1,54 @@
+package http
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type response struct {
+	Status int         `json:"status"`
+	Result interface{} `json:"result"`
+}
+
+func newResponse(data interface{}, status int) *response {
+	return &response{
+		Status: status,
+		Result: data,
+	}
+}
+
+func (resp *response) bytes() []byte {
+	data, _ := json.Marshal(resp)
+	return data
+}
+
+func (resp *response) string() string {
+	return string(resp.bytes())
+}
+
+func (resp *response) sendResponse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Accept-Control-Allow-Origin", "*")
+	w.WriteHeader(resp.Status)
+	_, _ = w.Write(resp.bytes())
+	log.Println(resp.string())
+}
+
+// 204
+func StatusNoContent()
+
+// 400
+func StatusBadRequest()
+
+// 404
+func StatusNotFound()
+
+// 405
+func StatusMethodNotAllowed()
+
+// 409
+func StatusConflict()
+
+// 500
+func StatusInternalServerError()
